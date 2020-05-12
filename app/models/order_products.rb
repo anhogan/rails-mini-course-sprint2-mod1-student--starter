ed0564c95e27149c8440373573e9fcedc401c334
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-12 11:32:55 -0400

    Setup custom routes for orders

---
+++ app/models/order_products.rb
@@ -0,0 +1,2 @@
+class OrderProducts < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :customers
+      resources :customers do
+        resources :orders, only: [:index, :create]
+      end
+
       resources :products, only: [:index, :show]
+      resources :orders do
+        resources :products, only: [:index, :create]
+        post "ship", to: "users#ship"
+      end
     end
   end
 end
